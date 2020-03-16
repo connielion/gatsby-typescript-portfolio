0395e0aa2049d55ce847ba6bc89b1bca66c92d7b
Connie Lai <clai011@ucr.edu>
2020-03-16 10:08:01 -0700

    Changed color, text, font

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,28 @@ import { Link } from "gatsby"
 import { HeaderProps } from "./types"
 import ThemeToggle from "../ThemeToggle"
 import styles from "./styles.module.scss"
+import {
+  FaTwitter as Twitter,
+  FaGithub as Github,
+  FaEnvelope as Mail,
+} from "react-icons/fa"
 
 const Header: FC<HeaderProps> = ({ siteTitle }) => (
   <header className={styles.header}>
     <h1 className={styles.h1}>
-      <Link to="/projects">projects</Link>
-      <Link to="/about">about</Link>
+      <Link className="mr-5 icon" to="/projects">
+        projects
+      </Link>
+      <Link className="mr-5 icon" to="/about">
+        about
+      </Link>
+      <Link className="mr-5 icon" to="https://github.com/connielion">
+        <Github />
+      </Link>
+
+      <Link className="mr-5 icon" to="mailto:connielion011@gmail.com">
+        <Mail />
+      </Link>
     </h1>
     <div className={styles.themeToggleContainer}>
       <ThemeToggle />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,36 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/Seo"
-import {
-  FaTwitter as Twitter,
-  FaGithub as Github,
-  FaEnvelope as Mail,
-} from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>
-      Hi, I am <span className="highlight">Connie</span>!
-    </h1>
+    <div className="container">
+      <div className="my-5"></div>
+      <div className="display-3">
+        Hello, I'm
+        <span className="text-primary"> Connie Lai.</span>
+      </div>
+      <div className="h2 code mt-4 mb-3">
+        <div className="h2 code mt-4 mb-3">
+          if (<span>you_Are_Hiring</span>) {"{"}
+        </div>
+      </div>
+      <div className="text-muted mx-5 my-4 h4 text-justify info">
+        <code>return </code>
+        "Here's the teamplayer, self-starter, and a highly curious Javascript
+        specialist you are lookin' for!"
+      </div>
+      <div className="h2 code mt-2 mb-3">{"}"}</div>
+      <div className="h2 code mt-4 mb-3">else {"{"}</div>
+      <div className="text-muted mx-5 my-4 h4 text-justify info">
+        <span className="hl">return </span>
+        <span className="text-warning">"Have a nice day~"</span>
+      </div>
+      <div className="h2 code mt-2 mb-3">{"}"}</div>
+    </div>
 
     <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}></div>
-    <Link className="mr-5 icon" to="https://github.com/connielion">
-      <Github />
-    </Link>
-
-    <a className="mr-5 icon" href="mailto:connielion011@gmail.com">
-      <Mail />
-    </a>
   </Layout>
 )
 
