a4c498cf119f41d2d3707e0428ddcf183ed08df7
Connie Lai <clai011@ucr.edu>
2020-03-16 13:23:46 -0700

    reinstalled dependencies after Gatsby issue #19827 and adding YML file

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,16 +1,47 @@
 import React, { FC } from "react"
 import { Link } from "gatsby"
-
+// import Card from "./Card"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
 const ProjectsPage: FC = () => {
+  //   const projectList = data.allProjectsYaml.edges
+
   return (
     <Layout>
       <SEO title="Projects" />
-      <div className="container-fluid"></div>
+      <div className="container-fluid">
+        <div className="row">
+          {/* {projectList.map(({ node }) => {
+            return (
+              <Card
+                cardTitle={node.title}
+                cardSubtitle={node.subtitle}
+                link={node.link}
+                key={node.id}
+              />
+            )
+          })} */}
+        </div>
+      </div>
     </Layout>
   )
 }
 
 export default ProjectsPage
+
+export const query = graphql`
+  {
+    allProjectsYaml {
+      edges {
+        node {
+          title
+          subtitle
+          link
+          id
+        }
+      }
+    }
+  }
+`
