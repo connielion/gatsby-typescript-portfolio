45880859bcfdf648ce8ee216b48e0be9eac04dd2
Connie Lai <clai011@ucr.edu>
2020-03-16 05:21:59 -0700

    Github and Mail icons added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/Seo"
+import {
+  FaTwitter as Twitter,
+  FaGithub as Github,
+  FaEnvelope as Mail,
+} from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
@@ -13,7 +18,13 @@ const IndexPage = () => (
     </h1>
 
     <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}></div>
-    <Link to="https://github.com/connielion">Github</Link>
+    <Link className="mr-5 icon" to="https://github.com/connielion">
+      <Github />
+    </Link>
+
+    <a className="mr-5 icon" href="mailto:connielion011@gmail.com">
+      <Mail />
+    </a>
   </Layout>
 )
 
