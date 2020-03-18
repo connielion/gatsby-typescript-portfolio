0f0280de6a6ae9c559b499676813914e6736d1aa
Connie Lai <clai011@ucr.edu>
2020-03-18 06:09:06 -0700

    Added dark background for front page section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,31 +9,43 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="container">
-      <div className="my-5"></div>
-      <div className="display-3">
-        Hello, I'm
-        <span className="text-primary"> Connie Lai.</span>{" "}
-        <button className="btn-warning btn">Resume</button>
-      </div>
-      <div className="h2 code mt-4 mb-3">
-        <div className="h2 code mt-4 mb-3">
-          if (<span>you_Are_Hiring</span>) {"{"}
+      <div className="row">
+        <div className="my-5"></div>
+        <div className="display-3">
+          Hello, I'm
+          <span className="text-primary"> Connie Lai.</span>{" "}
+          <button className="btn-warning btn">Resume</button>
         </div>
       </div>
-      <div className="text-muted mx-5 my-4 h4 text-justify info">
-        <code>return </code>
-        "Here's the teamplayer, self-starter, and a highly curious Javascript
-        specialist you are lookin' for!"
+      {/* Code Section */}
+      <div
+        style={{
+          backgroundColor: "#283341",
+          color: "whitesmoke",
+          padding: "10px",
+          borderRadius: "8px",
+        }}
+      >
+        <div className="h2 code mt-4 mb-3">
+          <div className="h2 code mt-4 mb-3">
+            if (<span>you_Are_Hiring</span>) {"{"}
+          </div>
+        </div>
+        <div className="mx-5 my-4 h4 text-justify info">
+          <code>return </code>
+          "Here's the teamplayer, self-starter, and a highly curious Javascript
+          specialist you are lookin' for!"
+        </div>
+        <div className="h2 code mt-2 mb-3">{"}"}</div>
+        <div className="h2 code mt-4 mb-3">else {"{"}</div>
+        <div className=" mx-5 my-4 h4 text-justify info">
+          <code>return </code>
+          <span>"Have a nice day~"</span>
+        </div>
+        <div className="h2 code mt-2 mb-3">{"}"}</div>
       </div>
-      <div className="h2 code mt-2 mb-3">{"}"}</div>
-      <div className="h2 code mt-4 mb-3">else {"{"}</div>
-      <div className="text-muted mx-5 my-4 h4 text-justify info">
-        <code>return </code>
-        <span className="text-secondary">"Have a nice day~"</span>
-      </div>
-      <div className="h2 code mt-2 mb-3">{"}"}</div>
     </div>
-
+    {/* end container */}
     <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}></div>
   </Layout>
 )
