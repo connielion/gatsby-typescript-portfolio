faf4a3acb3fa999dd0a55113303a032247a1b710
Connie Lai <clai011@ucr.edu>
2020-03-18 13:13:16 -0700

    Fixing nav css issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
         path: `./content/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `backgrounds`,
-        path: `${__dirname}/src/bg`, // wherever background images are stored
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
