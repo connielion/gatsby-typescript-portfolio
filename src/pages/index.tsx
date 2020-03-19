a88a1608ba5a79d652f7017b0254ddd02189eed6
Connie Lai <clai011@ucr.edu>
2020-03-19 10:02:59 -0700

    typing effect added to front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,27 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Image from "../components/Image"
 import SEO from "../components/Seo"
-import { IoMdOpen as Open } from "react-icons/io"
-import { Bounce, Flip } from "react-reveal"
+import { Slide, Fade } from "react-reveal"
+import ReactTypingEffect from "react-typing-effect"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="container">
       <div className="row">
-        <div className="display-4 intro">
-          <Bounce left>
-            <span className="intro"> Hello, I'm Connie.</span>{" "}
-            <a
-              className="btn-warning btn"
-              target="_blank"
-              href="https://drive.google.com/open?id=13r0l13AVT1VNREgyKcCyptmBrWZCv-Zc"
-            >
-              My Resume <Open />
-            </a>
-          </Bounce>
+        <div
+          className="display-4 intro"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Slide left>
+            <p className="intro">Hello, I'm Connie. </p>
+            {"  "}
+          </Slide>
+          <Slide right>
+            <div className="display-4 intro text-primary">
+              Full-stack engineer.
+            </div>
+          </Slide>
         </div>
       </div>
       {/* Code Section */}
@@ -42,14 +44,19 @@ const IndexPage = () => (
         </div>
         <div className="mx-5 my-4 h4 text-justify info">
           <code>return </code>
-          "Here's the teamplayer, self-starter, and a highly curious Javascript
-          specialist you are lookin' for!"
+          `Here's the teamplayer, self-starter, and a highly curious Javascript
+          specialist you are lookin' for!`
         </div>
         <div className="h2 code mt-2 mb-3">{"}"}</div>
         <div className="h2 code mt-4 mb-3">else {"{"}</div>
         <div className=" mx-5 my-4 h4 text-justify info">
           <code>return </code>
-          <span>"Have a nice day~"</span>
+          <ReactTypingEffect staticText="'Have a nice day!'" />
+          <br />
+          <br />
+          <span className="text-success">
+            // yeah, something gone wrong with navbar after deployment :(
+          </span>
         </div>
         <div className="h2 code mt-2 mb-3">{"}"}</div>
       </div>
