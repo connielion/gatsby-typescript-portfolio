31f0a26d4a0d232fea87cde9463db93b76a069a8
Connie Lai <clai011@ucr.edu>
2020-03-20 02:09:48 -0700

    Fixed href link, icons

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,47 +3,53 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
-import { FaPhone as Phone, FaLinkedin as LinkedIn } from "react-icons/fa"
+import {
+  FaPhone as Phone,
+  FaLinkedin as LinkedIn,
+  FaBomb as Bomb,
+} from "react-icons/fa"
 import { FiAtSign as At } from "react-icons/fi"
 import Rotate from "react-reveal/Rotate"
 import Clock from "react-live-clock"
+
 const ContactPage: FC = () => (
   <Layout>
     <SEO title="Contact" />
-    <div className="container">
-      <h3>
-        I'm based in California. Current time here is:{" "}
-        <Clock
-          format={"HH:mm:ss"}
-          ticking={true}
-          timezone={"US/Pacific"}
-          style={{ color: "orange" }}
-        />
-      </h3>
-      <ul className="contact-ul">
-        <Rotate top left>
-          <li>
-            <Phone />
-            <br />
-            My cell phone number is: <a href="tel:5592816498">559-281-6498</a>
-          </li>
-        </Rotate>
-        <Rotate top left>
-          <li>
-            <At />
-            <br />
-            My email is:{" "}
-            <a href="mailto:connielion011@gmail.com">connielion011@gmail.com</a>
-          </li>
-        </Rotate>
-        <Rotate top left>
-          <li>
-            <LinkedIn />
-            <br />
-            My LinkedIn profile: <a href="linkedin.com/in/connielion">here</a>
-          </li>
-        </Rotate>
-      </ul>
+    <div className="container d-flex">
+      <div className="row">
+        <b>
+          I'm based in California. Current time here is:{" "}
+          <Clock
+            format={"HH:mm:ss"}
+            ticking={true}
+            timezone={"US/Pacific"}
+            style={{ color: "orange" }}
+          />{" "}
+          .
+        </b>
+        <ul className="contact-ul">
+          <Rotate top left>
+            <li>
+              <Phone /> <a href="tel:5592816498">559-281-6498</a>
+            </li>
+          </Rotate>
+          <Rotate top left>
+            <li>
+              <At />{" "}
+              <a href="mailto:connielion011@gmail.com">
+                connielion011@gmail.com
+              </a>
+            </li>
+          </Rotate>
+          <Rotate top left>
+            <li>
+              <a href="https://linkedin.com/in/connielion">
+                <LinkedIn /> linkedin.com/in/connielion
+              </a>
+            </li>
+          </Rotate>
+        </ul>
+      </div>
     </div>
   </Layout>
 )
