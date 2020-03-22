d3f6ef813bb28391669b2e79e980528d3eaf867f
Connie Lai <clai011@ucr.edu>
2020-03-21 22:55:08 -0700

    Added Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,13 @@ module.exports = {
         path: `./content/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-118994416-2",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
