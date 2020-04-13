337e43b9dfb0acb86b1fb1c4cd2c98f53db01d29
Connie Lai <clai011@ucr.edu>
2020-04-12 23:16:48 -0700

    deleting branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,16 +11,18 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="container">
       <div className="row">
-        <Slide left>
-          <h3>Hello, I'm Connie Lai.</h3>
+        <Slide right>
+          <div class="h1">
+            <span className="text-primary">Connie Lai.</span>
+          </div>
           {"  "}
         </Slide>
-        <Slide right>
-          <h3 className="text-primary">
+        <Slide left>
+          <p className="h2">
             {" "}
             <br />
-            Full-stack Software engineer.
-          </h3>
+            <u>Full-stack</u> Software Engineer
+          </p>
         </Slide>
       </div>
       {/* <div className="row">
@@ -29,42 +31,6 @@ const IndexPage = () => (
         </Slide>
       </div> */}
       {/* Code Section */}
-      <div
-        className="scp info"
-        style={{
-          backgroundColor: "#283341",
-          color: "whitesmoke",
-          padding: "10px",
-          borderRadius: "8px",
-        }}
-      >
-        <div className="h4 info">
-          <div className="code mt-4 mb-3">
-            <span className="text-warning">// Hi :D</span>
-            <br />
-            if (<span>you_Are_Hiring</span>) {"{"}
-          </div>
-
-          <div className="mx-5 my-4 text-justify info">
-            <code>return </code>
-            `Here's the teamplayer, self-starter, and a highly curious engineer
-            you are lookin' for!`
-          </div>
-          <div className=" code mt-2 mb-3">{"}"}</div>
-          <div className=" code mt-4 mb-3">else {"{"}</div>
-          <div className=" mx-5 my-4 text-justify info">
-            <code>return </code>
-            'Have a wonderful day!'
-            <br />
-            <br />
-            <span className="text-warning">
-              // P.S. there's a weird bug on this page. I swear I googled over
-              10 times.
-            </span>
-          </div>
-          <div className="code mt-2 mb-3">{"}"}</div>
-        </div>
-      </div>
     </div>
     {/* end container */}
     <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}></div>
