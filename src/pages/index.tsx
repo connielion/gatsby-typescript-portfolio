f575c9ac7214501995d50b23a78e8a8cddebbc5f
Connie Lai <clai011@ucr.edu>
2020-04-15 15:37:49 -0700

    Removed image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="container">
-      <img
-        src="https://avatars3.githubusercontent.com/u/16534498?s=400&u=0cd9d3cebe935f2ccd9e2a09fd48af5a918aa341&v=4"
-        className="rounded mx-auto d-block pfp"
-        alt="Connie's photo"
-      />
       <div className="row d-flex justify-content-center">
         <Slide left>
           <p className="h1 display-2">Connie Lai</p>
